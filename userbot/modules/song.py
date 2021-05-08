0d6b802b87fe4a614cb1fa1e4df2d56ff6914014
brendsupport <79204947+brendsupport@users.noreply.github.com>
2021-05-08 04:49:14 +0400

    Update song.py

--- userbot/modules/song.py
+++ userbot/modules/song.py
@@ -14,46 +14,6 @@ from userbot.cmdhelp import CmdHelp
 from userbot.language import get_value
 LANG = get_value("song")
 
-async def brendmusic(cat, QUALITY, hello):
-    search = cat
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.add_argument("--test-type")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.youtube.com/results?search_query=" + search)
-    user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
-    for i in user_data:
-        video_link = i.get_attribute("href")
-        break
-    if not os.path.isdir("./temp/"):
-        os.makedirs("./temp/")
-    if not video_link:
-        await hello.edit(f"Bağışlayın. `{search}` mahnısını tapa bilmirəm ")
-        return
-    try:
-        command = (
-            'youtube-dl -o "./temp/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality '
-            + QUALITY
-            + " "
-            + video_link
-        )
-        os.system(command)
-    except Exception as e:
-        return await hello.edit(f"`Xəta:\n {e}`")
-    try:
-        thumb = (
-            'youtube-dl -o "./temp/%(title)s.%(ext)s" --write-thumbnail --skip-download '
-            + video_link
-        )
-        os.system(thumb)
-    except Exception as e:
-        return await hello.edit(f"`Xəta:\n {e}`")
-
-
 @register(outgoing=True, pattern="^.deez(\d*|)(?: |$)(.*)")
 async def deezl(event):
     if event.fwd_from:
@@ -96,52 +56,8 @@ async def deezl(event):
             await event.client.send_message(event.chat_id, f"`{sarkilar.buttons[sira][0].text}` | " + LANG['UPLOADED_WITH'], file=sarki.message)
             await event.delete()
 
+
 @register(outgoing=True, pattern="^.song(?: |$)(.*)")
-async def _(event):
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-    elif reply:
-        if reply.message:
-            query = reply.messag
-    else:
-        event = await event.edit("`Nə tapmalıyam? `")
-        return
-    event = await event.edit("`Hey...! Sənin mahnını tapıram....`")
-    await brendmusic(str(query), "128k", event)
-    l = glob.glob("./temp/*.mp3")
-    if l:
-        await event.edit("Mahnını tapdım yükləyirəm...📥")
-    else:
-        await event.edit(f"Bağışlayın ..! `{query}` ilə bir şey tapa bilmirəm")
-        return
-    thumbcat = glob.glob("./temp/*.jpg") + glob.glob("./temp/*.webp")
-    if thumbcat:
-        catthumb = thumbcat[0]
-    else:
-        catthumb = None
-    loa = l[0]
-    await bot.send_file(
-        event.chat_id,
-        loa,
-        force_document=False,
-        allow_cache=False,
-        caption=query,
-        thumb=catthumb,
-        supports_streaming=True,
-        reply_to=reply_to_id,
-    )
-    await event.delete()
-    os.system("rm -rf ./temp/*.mp3")
-    os.system("rm -rf ./temp/*.jpg")
-    os.system("rm -rf ./temp/*.webp")
-
-
-
-@register(outgoing=True, pattern="^.songu(?: |$)(.*)")
 async def port_song(event):
     if event.fwd_from:
         return
